701dee1f2a2b5d1ee6daa78533480fc7a63b89c6
zxtanzxzx <zxtan97@hotmail.com>
2021-01-28 11:00:46 +0800

    Update top_bar.blade.php

--- resources/views/top_bar.blade.php
+++ resources/views/top_bar.blade.php
@@ -1,6 +1,13 @@
 <div>
+    <a href="{{route('home')}}" >Home</a> &nbsp;
+    <a href="{{route('user')}}" >User</a> &nbsp;
+    <a href="{{route('product')}}" >Product</a> &nbsp;
+    <a href="{{route('manufacturer')}}" >Manufacturer</a>
+</div>
+
+{{-- <div>
     <a href="{{route('home')}}" target="_blank">Home</a> &nbsp;
     <a href="{{route('user')}}" target="_blank">User</a> &nbsp;
     <a href="{{route('product')}}" target="_blank">Product</a> &nbsp;
     <a href="{{route('manufacturer')}}" target="_blank">Manufacturer</a>
-</div>
+</div> --}}
